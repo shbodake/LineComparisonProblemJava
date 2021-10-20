7bebb8913efe6a1b19f136a84df5af971b993533
shbodake <sayalibodake28@gmail.com>
2021-10-20 23:57:46 +0530

    Added: calculated length of co-ordinates is added

--- src/com/blp/line/LineComparison.java
+++ src/com/blp/line/LineComparison.java
@@ -2,6 +2,12 @@ package com.blp.line;
 
 public class LineComparison {
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Problem");
+        System.out.println("Welcome to Line Comparison Program");
+        int x1,x2,y1,y2;
+        double len;
+        x1=1;y1=1;x2=4;y2=4;
+        len=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+        System.out.println("Length:"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+len);
     }
+
 }
